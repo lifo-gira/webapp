3b171783c03e85190e90acc8d99e8b0b9c5bcfb9
lifo-gira <130842622+lifo-gira@users.noreply.github.com>
2023-04-15 18:20:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from "./cube-2.png";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const domain = "http://localhost:8000/get-all-data";
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    const fetchAllIssues = () => {
+      fetch(domain)
+        .then((res) => res.json())
+        .then((data) => {
+          setValues(data);
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchAllIssues();
+  }, [values]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          {values === []
+            ? "No data is present to display at the moment"
+            : values[values.length - 1]}
+        </h3>
+        <div className="Container">
+          {values.map((val, key) => {
+            return <p>{val}</p>;
+          })}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
